63d6c60a6cff7dce12ca6d116655cb78b4b2c9e0
EmanElsayed <emoelsayed2192002@gmail.com>
2023-03-16 16:18:37 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1, s , sizeof(s) - 1);
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
+	write(1, str, sizeof(str)-1);
 	return (1);
 }
