e45d6bc977772a2c971e5ab21b1f811e49b8b8f5
EmanElsayed <emoelsayed2192002@gmail.com>
2023-03-16 16:24:17 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
- * Return: 1 Success
+  * main - program to print sentence on terminal with using unix simple operations
+  * Return: (1) Success
  */
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
-	write(1 ,str ,sizeof(str) - 1);
+	write(1, str, sizeof(str)-1);
 	return (1);
 }
