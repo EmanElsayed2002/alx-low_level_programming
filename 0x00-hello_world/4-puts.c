402c03e7fa4bd567513e0469857eb7fc42cdb463
EmanElsayed <emoelsayed2192002@gmail.com>
2023-03-16 15:21:27 -0700

    dicumentation style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/*
+ *description ffor main function 
+ * use puts function to print message 
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
