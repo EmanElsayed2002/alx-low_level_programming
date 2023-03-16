315aff0782fa154438535ccfa2e5fe8f389c9fc6
EmanElsayed <emoelsayed2192002@gmail.com>
2023-03-16 15:51:30 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,6 +14,5 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
-        /* value 0 means there is no errors */
 	return (0);
 }
