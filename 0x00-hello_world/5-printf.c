58722ebd4eb4892533f96babf90ab0e682186310
EmanElsayed <emoelsayed2192002@gmail.com>
2023-03-16 15:38:43 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print  the program prints a message
+ *
+ * Description: using puts function to print a message
+ *
+ * Return: 0 means there is no errors
+ */
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	/* value 0 means there is no errors */
+	return (0);
+}
