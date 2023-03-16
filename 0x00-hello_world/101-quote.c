39a1b77a6e24b6906e9b5aa16dbd44c019112863
EmanElsayed <emoelsayed2192002@gmail.com>
2023-03-16 16:15:12 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: 1 Success
+ */
+int main(void)
+{
+	char s[] = "and that piece of art is useful
+                        \" - Dora Korpar, 2015-10-19\n";
+	write(s);
+	return (1);
+}
