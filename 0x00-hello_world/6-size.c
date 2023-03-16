fb76dc4ff29e7534da44efeefc70acd3ca3d4615
EmanElsayed <emoelsayed2192002@gmail.com>
2023-03-16 15:53:25 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /*
- * main : write c program to print size of types on the computer
+ * main -  write c program to print size of types on the computer
  *
  * Description : print the size of varios data types in computer
- *
  * Return : return 0 means there is no errors in program
  */
 int main(void)
